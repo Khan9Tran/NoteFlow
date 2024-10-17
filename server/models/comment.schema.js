690179326657672483d9762fec565df30cb6da95
tamnguyen1411 <chuotvip233@gmail.com>
2024-10-17 09:38:01 +0700

    schema

---
+++ server/models/comment.schema.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+
+const CommentSchema = new Schema({
+    taskId: { type: Schema.Types.ObjectId, ref: 'Task', required: true },
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    text: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
+  
+  module.exports = mongoose.model('Comment', CommentSchema);
+  
