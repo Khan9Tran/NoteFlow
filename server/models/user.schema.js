9153c924d92e048fccf192153f1b8a2b4f12f1e4
tamnguyen1411 <chuotvip233@gmail.com>
2024-10-17 11:27:33 +0700

    bỏ proPic

--- server/models/user.schema.js
+++ server/models/user.schema.js
@@ -7,7 +7,7 @@ const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
   googleId: { type: String },
   password: { type: String },
-  profilePicture: { type: String , required: true},
+  profilePicture: { type: String },
   workspaces: [{ type: Schema.Types.ObjectId, ref: 'Workspace' }],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
