279e2b14c94f7b6baaf917b63caec0cdf99948a6
tamnguyen1411 <chuotvip233@gmail.com>
2024-10-18 11:53:58 +0700

    auth middleware

--- server/api/controllers/users/router.js
+++ server/api/controllers/users/router.js
@@ -1,11 +1,11 @@
-import * as express from 'express';
-import { registerUser, loginUser } from './controller.js';
-import validate from '../../middlewares/validate.js';
+import * as express from "express";
+import { registerUser, loginUser } from "./controller.js";
+import validate from "../../middlewares/validate.js";
 import { userRegisterRequest } from "../../validators/user/registerRequest.js";
 import { userLoginRequest } from "../../validators/user/loginRequest.js";
-
+import authMiddleware from "../../../security/auth-middleware.js";
 
 export default express
   .Router()
-  .post('/', validate(userRegisterRequest),registerUser)
-  .post('/login', validate(userLoginRequest), loginUser);
+  .post("/", validate(userRegisterRequest), registerUser)
+  .post("/login", validate(userLoginRequest), loginUser);
